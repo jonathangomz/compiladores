c766dff5a1f92e022aed29127c5bf25c1d7b72a6
Jonathan Gómez <jonicgp97@hotmail.com>
2018-02-26 10:38:36 -0600

    Se inicializó un nuevo proyecto para corregir error con SharpRaven

---
+++ ConsoleApp1/Lexer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+internal class Lexer
+{
+    public int index = 0;
+    public int edo = 0;
+
+    public string text = "";
+    public TokenType tokenType = TokenType.TOKEN_NONE;
+
+    public Regex simbol = new Regex(@"[+, *,\/,\-, (, ), [,\],{,}]");
+
+    public Lexer()
+    {
+    }
+
+    public Token NextToken(string input, List<string> reservedWords)
+    {
+        for (int i = index; i < input.Length; i++)
+        {
+            char c = input[i];
+            Console.Write("c: " + c + " edo: " + edo + " index: " + index + " => ");
+            while ((c == ' ' || c == '\t') && edo == 0)
+            {
+                i++;
+            }
+            if (i >= input.Length)
+            {
+                return new Token();
+            }
+            // **SUM**
+            if (edo == 0 && c == '+')
+            {
+                edo = 0;
+                return new Token(TokenType.SUM, input.Substring(index, ++index - i));
+            }
+            // **RES**
+            if (edo == 0 && c == '-')
+            {
+                edo = 0;
+                return new Token(TokenType.RES, input.Substring(index, ++index - i));
+            }
+            // **DIV**
+            if (edo == 0 && c == '/')
+            {
+                edo = 0;
+                return new Token(TokenType.DIV, input.Substring(index, ++index - i));
+            }
+            // **MUL**
+            if (edo == 0 && c == '*')
+            {
+                edo = 0;
+                return new Token(TokenType.MUL, input.Substring(index, ++index - i));
+            }
+            // **ID**
+            // Si es un ID y está al final de la línea
+            if (((edo == 0 && Char.IsLetter(c)) || (edo == 3 && Char.IsLetterOrDigit(c))) && i + 1 >= input.Length)
+            {
+                i++;
+                edo = 0;
+                text = input.Substring(index, i - index).Trim();
+                tokenType = TokenType.ID;
+                index = i;
+                return new Token(tokenType, text);
+            }
+            // Si comienza un ID o continua sigue al siguiente caracter
+            if ((edo == 0 && Char.IsLetter(c)) || (edo == 3 && Char.IsLetterOrDigit(c)))
+            {
+                edo = 3;
+                continue;
+            }
+            //Si el siguiente caracter es otro simbolo regresa el texto del ID
+            if (edo == 3 && simbol.IsMatch(c.ToString()))
+            {
+                edo = 0;
+                text = input.Substring(index, i - index).Trim();
+                tokenType = TokenType.ID;
+                index = i;
+                return new Token(tokenType, text);
+            }
+            // **NUM**
+            // Si es un NUM y está al final de la línea
+            if (((edo == 0 && Char.IsNumber(c)) || (edo == 5 && Char.IsNumber(c))) && i + 1 >= input.Length)
+            {
+                i++;
+                edo = 0;
+                text = input.Substring(index, i - index).Trim();
+                tokenType = TokenType.NUM;
+                index = i;
+                return new Token(tokenType, text);
+            }
+            // Si comienza un NUM o continua sigue al siguiente caracter
+            if ((edo == 0 || edo == 5) && Char.IsNumber(c))
+            {
+                edo = 5;
+                continue;
+            }
+            //Si el siguiente caracter es otro simbolo regresa el texto del NUM
+            if (edo == 5 && simbol.IsMatch(c.ToString()))
+            {
+                edo = 0;
+                text = input.Substring(index, i - index).Trim();
+                tokenType = TokenType.NUM;
+                index = i;
+                return new Token(tokenType, text);
+            }
+        }
+        return new Token();
+    }
+
+    public void getTokens()
+    {
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SharpRaven;
+using SharpRaven.Data;
+
+public class Compiladores
+{
+
+    public static List<String> reservedWords = new List<String>();
+    public static String input;
+
+    public Compiladores()
+    {
+    }
+
+    public static void Main()
+    {
+        var ravenClient = new RavenClient("https://b75d8a0984ba48c39a2ae86070f9eb04:0ccab29913eb4a34b8c0013ab7036637@sentry.io/291909");
+
+        //ravenClient.Capture(new SentryEvent("Hello World!"));
+
+        reservedWords.Add("if");
+        input = "a+b*13/2";
+        Lexer l = new Lexer();
+        Token t = l.NextToken(input, reservedWords);
+        while (t.Type != TokenType.TOKEN_NONE)
+        {
+            Console.WriteLine("" + t.Text + t.Type);
+            t = l.NextToken(input, reservedWords);
+        }
+        Console.ReadKey();
+    }
+}
