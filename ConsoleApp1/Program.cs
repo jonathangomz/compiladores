349629fbfac7128f9040fc68b7f5bf343ccd3abf
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-01 11:38:54 -0600

    Se agregó el manejador de errores con SharpRaven. No se si funciona xD

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,14 +19,36 @@ public class Compiladores
         var ravenClient = new RavenClient("https://b75d8a0984ba48c39a2ae86070f9eb04:0ccab29913eb4a34b8c0013ab7036637@sentry.io/291909");
 
         //ravenClient.Capture(new SentryEvent("Hello World!"));
+        try
+        {
+            input = "a+b*123";
+            reservedWords.Add("if");
 
-        ListaLexer l = new ListaLexer();
+            Lexer lex = new Lexer();
+            List<Token> listTokens = lex.ListOfToken(input, reservedWords);
 
-        input = "a+b*123";
-        reservedWords.Add("if");
-
-        List<Token> test = l.getListaToken(input, reservedWords);
-        Console.WriteLine(test.GetRange(0,1));
+            listTokens.ForEach(delegate (Token tok)
+            {
+                Console.WriteLine(tok.Text + " => " + tok.Type);
+            });
+        }
+        catch (Exception ex)
+        {
+            string ask = "n";
+            Console.WriteLine("¿Guardar el error? y/n (Default no)");
+            do
+            {
+                ask = Console.ReadLine();
+            } while (!ask.Equals("y") && !ask.Equals("n"));
+            if (ask.Equals("y"))
+            {
+                ravenClient.Capture(new SentryEvent(ex));
+                ravenClient.ErrorOnCapture = exception =>
+                {
+                    Console.WriteLine("Ocurrió un error al capturar el... error.");
+                };
+            }
+        }
         Console.ReadKey();
     }
 }
