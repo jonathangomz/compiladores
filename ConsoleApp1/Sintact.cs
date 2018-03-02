d73791297e70b2e7410f3a5e7c56b0ea7788aa2b
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-02 11:24:11 -0600

    Se creó la clase del analizador sintáctico.

---
+++ ConsoleApp1/Sintact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Sintact
+{
+    string input; 
+    List<string> reservedWords;
+
+    public Sintact(string input, List<string> reservedWords)
+    {
+        this.input = input;
+        this.reservedWords = reservedWords;
+    }
+    public Sintact(){ }
+
+    public void Expression()
+    {
+        Termino();
+        Token t = Lexer.NextToken(input, reservedWords);
+    }
+
+    public void Termino()
+    {
+        Factor();
+    }
+
+    private void Factor()
+    {
+
+    }
+}
