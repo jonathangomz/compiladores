a2a8a74ba1dd8c596de4193151d03b81aa6c0ef1
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-05 11:54:11 -0600

    Se creó la clase Compiler. Trabajando en ella...

---
+++ ConsoleApp1/Compiler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+internal class Compiler
+{
+    string input;
+    List<string> reservedWords;
+
+    Lexer lex   = new Lexer();
+    Parser par  = new Parser();
+
+    public Compiler(string input, List<string> reservedWords)
+    {
+        this.input = input;
+        this.reservedWords = reservedWords;
+    }
+}
