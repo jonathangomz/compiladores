fc0a135e8e8bc19a2ac4f9d2b8d69daac1c804a5
Rorschach <Rorschach@DESKTOP-OAIPHTM>
2018-03-05 09:13:17 -0600

    Continuó el desarrollo de los métodos Expression, Termino y Factor. --ERROR--

--- ConsoleApp1/Sintact.cs
+++ ConsoleApp1/Sintact.cs
@@ -1,31 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 
-internal class Sintact
+internal class Parser
 {
     string input; 
     List<string> reservedWords;
+    bool huboerror = false; // Si ocurre un error no sigue avanzando
+    bool advance = false;
+    Lexer l = new Lexer();
+    Token tok;
 
-    public Sintact(string input, List<string> reservedWords)
+    /* Constructores */
+    public Parser(string input, List<string> reservedWords)
     {
         this.input = input;
         this.reservedWords = reservedWords;
+        this.tok = l.NextToken(input, reservedWords);
     }
-    public Sintact(){ }
+    public Parser(){ }
 
-    public void Expression()
+ /* Métodos de la Clase */
+    public float Expression()
     {
-        Termino();
-        Token t = Lexer.NextToken(input, reservedWords);
+        if (huboerror) return 0;
+        do
+        {
+            Termino();
+            if (tok.Type == TokenType.SUM)
+                advance = true;
+            tok = l.NextToken(input, reservedWords);
+        } while(tok.Type == TokenType.SUM);
+        return 1;
     }
 
-    public void Termino()
+    public float Termino()
     {
-        Factor();
+        if (huboerror) return 0;
+        do
+        {
+            Factor();
+            if (tok.Type == TokenType.MUL)
+                advance = true;
+            tok = l.NextToken(input, reservedWords);
+        } while (tok.Type == TokenType.MUL);
+        return 1;
     }
 
-    private void Factor()
+    private float Factor()
     {
+        if (huboerror)
+            return 0;
 
+        if (tok.Type != TokenType.ID)
+        {
+            Console.WriteLine("ERR");
+            huboerror = true;
+            System.Environment.Exit(-1);
+            return 0;
+        }
+        else
+            return 1;
     }
 }
