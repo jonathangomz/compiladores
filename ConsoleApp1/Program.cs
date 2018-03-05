5fbfcc8a3aa5f4e086aa4f97f7e1c2c9a9c09a6e
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-05 11:21:36 -0600

    Se implementó Regex, se dividió en métodos y se agregaron mejoras

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 using SharpRaven;
 using SharpRaven.Data;
@@ -10,6 +11,9 @@ public class Compiladores
     public static List<String> reservedWords = new List<String>();
     public static String input;
 
+    public static Regex yes = new Regex(@"(?i)^(y|Y|YES|Yes|yes)$");
+    public static Regex no = new Regex(@"(?i)^(n|N|NO|No|no)$");
+
     public Compiladores()
     {
     }
@@ -18,40 +22,106 @@ public class Compiladores
     {
         var ravenClient = new RavenClient("https://b75d8a0984ba48c39a2ae86070f9eb04:0ccab29913eb4a34b8c0013ab7036637@sentry.io/291909");
 
-        //ravenClient.Capture(new SentryEvent("Hello World!"));
         try
         {
-            input = "a+b";
-            reservedWords.Add("if");
-            Parser par = new Parser(input, reservedWords); 
-            Lexer lex = new Lexer();
-            List<Token> listTokens = lex.ListOfToken(input, reservedWords);
-
-            float r = par.Expression();
-            listTokens.ForEach(delegate (Token tok)
+            /* PROGRAMA PRINCIPAL */
+            while (true)
             {
-                Console.WriteLine(tok.Text + " => " + tok.Type);
-            });
+                Console.WriteLine("Introduce el comando: ");
 
-            Console.WriteLine(r);
+                // Se introduce un comando
+                input = Console.ReadLine();
+
+                // Se crea el listado de palabras reservadas (no terminado)
+                reservedWords.Add("if");
+
+                // Se inicializan las clases que ejecutarán el chequeo
+                Parser par = new Parser(input, reservedWords);
+                Lexer lex = new Lexer();
+
+                // Se obtiene la lista de Tokens del texto ingresado
+                List<Token> listTokens = lex.ListOfToken(input, reservedWords);
+
+                // Se imprimen los Tokens para comparación
+                listTokens.ForEach(delegate (Token tok)
+                {
+                    Console.WriteLine(tok.Text + " => " + tok.Type);
+                });
+
+                // Se ejecuta el chequeo sintáctico
+                float r = par.Expression();
+
+                // Se imprime el resultado. En caso de ser 1 la sintaxis es correcta, de ser 0 es incorrecta
+                Console.WriteLine("Respuesta => "+r);
+
+                // Pregunta para salir o continuar
+                Console.ReadKey();
+                AskToExit();
+            }
+            /*FIN DE PROGRAMA PRINCIPAL */
         }
         catch (Exception ex)
         {
-            string ask = "n";
-            Console.WriteLine("¿Guardar el error? y/n (Default no)");
-            do
+            Console.WriteLine("ERR => "+ex.Message);
+            SaveError(ravenClient, ex);
+        }
+    }
+
+    private static void Exit()
+    {
+        Environment.Exit(-1);
+    }
+
+    private static void AskToExit()
+    {
+        do
+        {
+            Console.WriteLine("¿Desea ingresar otro comando? y/n");
+            string ansStr = Console.ReadLine();
+            if (no.IsMatch(ansStr))
+                Exit();
+            else if (yes.IsMatch(ansStr))
             {
-                ask = Console.ReadLine();
-            } while (!ask.Equals("y") && !ask.Equals("n"));
-            if (ask.Equals("y"))
+                Console.WriteLine(" ---- NUEVO COMANDO ----");
+                break;
+            }
+            else
+                Console.WriteLine("No se reconoce respuesta => " + ansStr);
+        } while (true);
+    }
+
+    private static void SaveError(RavenClient ravenClient, Exception ex)
+    {
+        // Pregunta si se desea guardar el error
+        string ask;
+        while(true)
+        {
+            Console.WriteLine("¿Guardar el error? y/[n]");
+            ask = Console.ReadLine();
+
+            // Si se desea guardar
+            if (yes.IsMatch(ask))
             {
+                // Se envía el error
                 ravenClient.Capture(new SentryEvent(ex));
+
+                // Si ocurre un error al enviar se muestra
                 ravenClient.ErrorOnCapture = exception =>
                 {
                     Console.WriteLine("Ocurrió un error al capturar el... error.");
                 };
+                break;
             }
+
+            // Si no se desea guardar
+            else if (no.IsMatch(ask))
+            {
+                Console.WriteLine("No se guardó");
+                break;
+            }
+
+            // Si no se reconoce la respuesta
+            else Console.WriteLine("No se reconoce comando ingresado => " + ask);
         }
-        Console.ReadKey();
     }
 }
