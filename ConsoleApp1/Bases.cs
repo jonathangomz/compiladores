6bed0199c1c07e3a47a53786066e080569dd52af
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-06 10:53:34 -0600

    Se creó la clase Base con ciertos parámetros esceciales del compilador

---
+++ ConsoleApp1/Bases.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Bases
+{
+    protected string input;
+    protected List<string> reservedWords;
+    protected Token tokErr = new Token();
+
+    protected bool huboerror = false;
+
+    public virtual float Error(dynamic tok, string msg)
+    {
+        tokErr = tok;
+        huboerror = true;
+        Console.WriteLine("ERR => " + tokErr.Type + ": " + msg);
+        return -1;
+    }
+}
