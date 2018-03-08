e95e61d39bba4f3f5d4cfa835b09dfed2e328e9b
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-08 11:58:18 -0600

    Se añadieron Exceptions

--- ConsoleApp1/Bases.cs
+++ ConsoleApp1/Bases.cs
@@ -5,15 +5,49 @@ internal class Bases
 {
     protected string input;
     protected List<string> reservedWords;
-    protected Token tokErr = new Token();
+    public Token tokErr = new Token();
 
     protected bool huboerror = false;
 
-    public virtual float Error(dynamic tok, string msg)
+    public virtual float Error(Exception ex, dynamic tok, string msg)
     {
+
         tokErr = tok;
         huboerror = true;
         Console.WriteLine("ERR => " + tokErr.Type + ": " + msg);
         return -1;
+    }
+
+    public class EOLException: Exception
+    {
+        public EOLException(String message): base(message)
+        {
+        }
+
+        public EOLException(String message, Exception innerException): base(message, innerException)
+        {
+        }
+    }
+
+    public class ChoosingTokenException: Exception
+    {
+        public ChoosingTokenException(String message) : base(message)
+        {
+        }
+
+        public ChoosingTokenException(String message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    public class FactorException : Exception
+    {
+        public FactorException(string message) : base(message)
+        {
+        }
+
+        public FactorException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
     }
 }
