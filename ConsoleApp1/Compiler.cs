69a1004cb04f85bce4a65ec383d9627ed924787a
Rorschach <Rorschach@DESKTOP-OAIPHTM>
2018-03-09 16:01:46 -0600

    Segundo constructor que solo recibe el input

--- ConsoleApp1/Compiler.cs
+++ ConsoleApp1/Compiler.cs
@@ -4,9 +4,28 @@ using System.Collections.Generic;
 internal class Compiler: Parser
 {
 
-    public Compiler(string input, List<string> reservedWords)
+    public Compiler(string input, Dictionary<string, TokenType> reservedWords)
     {
         this.input = input;
         this.reservedWords = reservedWords;
+    }
+
+    public Compiler(string input)
+    {
+        this.input = input;
+        reservedWords = new Dictionary<string, TokenType>()
+        {
+            { "if",TokenType.IF },
+            { "else",TokenType.ELSE },
+            { "do",TokenType.DO },
+            { "while",TokenType.WHILE },
+            { "switch",TokenType.SWITCH },
+            { "case",TokenType.CASE },
+            { "for",TokenType.FOR },
+            { "return",TokenType.RETURN },
+            { "sen",TokenType.SEN },
+            { "cos",TokenType.COS },
+            { "tan",TokenType.TAN }
+        };
     }
 }
