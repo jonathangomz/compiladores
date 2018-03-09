a8cabda5e3ea4825bbf46307200edd8e9ea25d25
Jonathan Gómez <jonicgp97@hotmail.com>
2018-03-09 12:02:48 -0600

    Se modificarón a una Exception por nivel

--- ConsoleApp1/CompilerBase.cs
+++ ConsoleApp1/CompilerBase.cs
@@ -5,48 +5,26 @@ internal class CompilerBase
 {
     protected string input;
     protected List<string> reservedWords;
-    public Token tokErr = new Token();
 
-    protected bool huboerror = false;
+    public Token tokErr = new Token(); //No se ocupa en el contexto actual del proyecto 
+    protected bool huboerror = false;  //Hasta ahora solo se ocupa en la clase Parser
 
-    public virtual float Error(Exception ex, dynamic tok, string msg)
+    public class LexerException: Exception
     {
-
-        tokErr = tok;
-        huboerror = true;
-        Console.WriteLine("ERR => " + tokErr.Type + ": " + msg);
-        return -1;
-    }
-
-    public class EOLException: Exception
-    {
-        public EOLException(String message): base(message)
+        public LexerException(String message): base("ERR>> "+message)
         {
         }
-
-        public EOLException(String message, Exception innerException): base(message, innerException)
+        public LexerException(String message, Exception innerException): base("ERR>> " + message, innerException)
         {
         }
     }
 
-    public class ChoosingTokenException: Exception
+    public class ParserException: Exception
     {
-        public ChoosingTokenException(String message) : base(message)
+        public ParserException(String message) : base("ERR>> " + message)
         {
         }
-
-        public ChoosingTokenException(String message, Exception innerException) : base(message, innerException)
-        {
-        }
-    }
-
-    public class FactorException : Exception
-    {
-        public FactorException(string message) : base(message)
-        {
-        }
-
-        public FactorException(string message, Exception innerException) : base(message, innerException)
+        public ParserException(String message, Exception innerException) : base("ERR>> " + message, innerException)
         {
         }
     }
